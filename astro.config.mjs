387eb71861b4a1be64934b6c97c522c7e79c3e04
Klas Näsman <klas.nasman@gmail.com>
2026-05-29 18:36:19 +0200

    seo fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://klasnasman.com",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   prefetchAll: true,
 });
