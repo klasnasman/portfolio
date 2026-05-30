0a321e4a6ee4765d26d318a7d7a1f4d456baf8cc
Klas Näsman <klas.nasman@gmail.com>
2026-05-30 20:09:25 +0200

    table fix

--- src/assets/icons/tree.tsx
+++ src/assets/icons/tree.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Tree: React.FC<React.SVGProps<SVGSVGElement>> = props => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' fill='none' width='22' height='22' viewBox='0 0 24 24' {...props}>
+    <svg xmlns='http://www.w3.org/2000/svg' fill='none' width='20' height='20' viewBox='0 0 24 24' {...props}>
       <path
         fill='none'
         stroke='currentColor'
