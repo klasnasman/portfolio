aa7940b29d2371d6dbb979780565731389bfe14d
Klas Näsman <klas.nasman@gmail.com>
2026-06-03 09:46:49 +0200

    smaller icons

--- src/assets/icons/tree.tsx
+++ src/assets/icons/tree.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Tree: React.FC<React.SVGProps<SVGSVGElement>> = props => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' fill='none' width='20' height='20' viewBox='0 0 24 24' {...props}>
+    <svg xmlns='http://www.w3.org/2000/svg' fill='none' width='18' height='18' viewBox='0 0 24 24' {...props}>
       <path
         fill='none'
         stroke='currentColor'
         strokeLinecap='round'
         strokeLinejoin='round'
-        strokeWidth='2'
+        strokeWidth='2.5'
         d='M8 19a4 4 0 0 1-2.24-7.32A3.5 3.5 0 0 1 9 6.03V6a3 3 0 1 1 6 0v.04a3.5 3.5 0 0 1 3.24 5.65A4 4 0 0 1 16 19Zm4 0v3'
       ></path>
     </svg>
